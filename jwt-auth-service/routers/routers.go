bb72d923467b89ebf1da703bf2b3532b1db8e5e2
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2024-06-02 21:59:19 +0100

    adicionar delete auth-service

--- jwt-auth-service/routers/routers.go
+++ jwt-auth-service/routers/routers.go
@@ -10,5 +10,7 @@ func InitRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/register", controllers.CreateUser).Methods("POST")
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
+	router.HandleFunc("/user/{id}", controllers.UpdateUser).Methods("PUT")
+	router.HandleFunc("/user/{id}", controllers.DeleteUser).Methods("DELETE")
 	return router
 }
