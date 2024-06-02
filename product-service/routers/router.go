171799b7fbe060545543f40c779ec2b14457da67
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2024-06-02 19:00:55 +0100

    update commits

--- product-service/routers/router.go
+++ product-service/routers/router.go
@@ -10,5 +10,6 @@ func InitRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/products", controllers.CreateProduct).Methods("POST")
 	router.HandleFunc("/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/products/{id}", controllers.UpdateProduct).Methods("PUT")
 	return router
 }
